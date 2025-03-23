bb13653f37fd67032444c6b40bca913d64823b41
carlajhenifermorais <carlajhenifermorais@gmail.com>
2025-03-23 18:24:52 -0300

    commit inicial

---
+++ Questão1.2.py
@@ -0,0 +1,7 @@
+#  Escreva os mesmos códigos acompanhando os Vídeos 3 e 4 usando a linguagem Python. Depois salve o arquivo .py que você programou.
+
+x = float(input("Informe o primeiro número: "))
+y = float(input("Informe o segundo número: "))
+s = x + y
+p = x != y
+print(f"O valor da soma é {s} {p}")
